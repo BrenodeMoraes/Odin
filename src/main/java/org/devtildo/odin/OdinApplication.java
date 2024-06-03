0290526c06b4750122b07bb8bb9e04b8df231ec9
tildo <breno.moraes1809@gmail.com>
2024-06-02 21:41:41 -0300

    start: create project

---
+++ src/main/java/org/devtildo/odin/OdinApplication.java
@@ -0,0 +1,13 @@
+package org.devtildo.odin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdinApplication.class, args);
+    }
+
+}
